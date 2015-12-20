52de03efbeb598d4c737cc119011aa197b112d9e
Jakob Rasmussen <jakob@jakobra.com>
2015-12-20 17:51:53 +0100

    Added handlebars

--- media_manager.js
+++ media_manager.js
@@ -1,13 +1,17 @@
 var express = require('express');
+var exphbs  = require('express-handlebars');
 var app = express();
 
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
 app.get('/', function (req, res) {
-  res.send('Media manager');
+	res.render('home');
 });
 
 var server = app.listen(3600, function () {
-  var host = server.address().address;
-  var port = server.address().port;
+	var host = server.address().address;
+	var port = server.address().port;
 
-  console.log('Example app listening at http://%s:%s', host, port);
+	console.log('Example app listening at http://%s:%s', host, port);
 });
